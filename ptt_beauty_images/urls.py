7e6d013e30bc3df304c7cbbce824f8b2b0339033
Victor <32451715+otaku0929@users.noreply.github.com>
2017-10-15 01:44:24 +0800

    Update urls.py

--- ptt_beauty_images/urls.py
+++ ptt_beauty_images/urls.py
@@ -13,15 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url,include
+from django.conf.urls import url
+#,include
 # from django.contrib import admin
-from images.views import *
+#from images.views import *
+from . import views
 from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register(r'image', ImageViewSet)
 
 urlpatterns = [
     #url(r'^admin/', admin.site.urls),
-    url(r'^$', *.index, name='index'),
-    url(r'^api/', include(router.urls)),
+    url(r'^$', views.index, name='index'),
+    #url(r'^api/', include(router.urls)),
 ]
