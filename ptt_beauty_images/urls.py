7185c060b64a7941633fb805c9a35eb836d4f80c
Victor <32451715+otaku0929@users.noreply.github.com>
2017-10-15 02:21:00 +0800

    Update urls.py

--- ptt_beauty_images/urls.py
+++ ptt_beauty_images/urls.py
@@ -22,6 +22,6 @@ router.register(r'image', ImageViewSet)
 
 urlpatterns = [
     #url(r'^admin/', admin.site.urls),
-    url(r'^$', images.views.index, name='index'),
+    url(r'^$', urls.index),
     url(r'^api/', include(router.urls)),
 ]
