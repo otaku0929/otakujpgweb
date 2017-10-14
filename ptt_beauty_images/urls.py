4e2b5ca27e2b16996f389bc31798047a5955b3cb
Victor <32451715+otaku0929@users.noreply.github.com>
2017-10-15 02:38:47 +0800

    Update urls.py

--- ptt_beauty_images/urls.py
+++ ptt_beauty_images/urls.py
@@ -15,13 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 # from django.contrib import admin
-from images.views import image
+from images.views import *
 from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register(r'image', ImageViewSet)
 
 urlpatterns = [
     #url(r'^admin/', admin.site.urls),
-    url(r'^$', image.index),
+    url(r'^$', index),
     url(r'^api/', include(router.urls)),
 ]
